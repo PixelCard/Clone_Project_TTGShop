e1113a155ccf8864dd91b3311cc369c32616a0c5
Thanh <nguyenthanhtoan8544@gmail.com>
2024-11-07 21:38:35 +0700

    add Login page and Product History and CheckOutPage

---
+++ WebBanMayTinh/Controllers/CheckoutPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanMayTinh.Controllers
+{
+    public class CheckoutPageController : Controller
+    {
+        // GET: CheckoutPage
+        public ActionResult CheckoutPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanMayTinh/Controllers/ProductHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanMayTinh.Controllers
+{
+    public class ProductHistoryController : Controller
+    {
+        // GET: ProductHistrory
+        public ActionResult ProductHistory()
+        {
+            return View();
+        }
+    }
+}
