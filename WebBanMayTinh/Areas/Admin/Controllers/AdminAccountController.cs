920c6749295e0e881ff0a4bd414c114589bd0536
DoAnhViet <anhziett@gmail.com>
2024-11-24 20:49:25 +0700

    Update login, Admin, regisster

---
+++ WebBanMayTinh/Areas/Admin/Controllers/AdminAccountController.cs
@@ -0,0 +1,59 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanMayTinh.Models;
+using WebBanMayTinh.Shared.Constants;
+
+namespace WebBanMayTinh.Areas.Admin.Controllers
+{
+    public class AdminAccountController : Controller
+    {
+        private readonly  Web_Ban_May_TinhEntities dbContext = new Web_Ban_May_TinhEntities();
+        private string currentFilter = "";
+        public ActionResult Index(string searchString = null, int? page = null)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            // Load list users theo role cua nguoi dung hien tai
+            // Admin - Load all
+            // Staff - Load customers
+            // Manager - Load customers + staffs
+            string role = Session[SessionDataKeys.UserRole].ToString();
+
+
+            IQueryable<Customer> customers = dbContext.Customers
+                .Include("User");
+
+            if (role == UserRoleConstants.Staff)
+            {
+                customers = customers.Where(c => c.User.UserRole == UserRoleConstants.Customer);
+            }
+            else if  (role == UserRoleConstants.Manager)
+            {
+                customers = customers
+                    .Where(c => c.User.UserRole == UserRoleConstants.Customer || c.User.UserRole == UserRoleConstants.Staff);
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            
+            var data = customers
+                .OrderBy(c => c.CustomerID)
+                .ToPagedList(pageNumber, pageSize);
+            
+            return View(data);
+        }
+    }
+}
--- WebBanMayTinh/Areas/Admin/Controllers/AuthController.cs
+++ WebBanMayTinh/Areas/Admin/Controllers/AuthController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebBanMayTinh.Models.ViewModel;
+using WebBanMayTinh.Shared.Constants;
 
 namespace WebBanMayTinh.Areas.Admin.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly Web_Ban_May_TinhEntities db = new Web_Ban_May_TinhEntities();
         // GET: Admin/Auth
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var hashedPassword = HashingHelper.HashPassword(model.Password);
+
+                var user = db.Users.SingleOrDefault(u => u.Email == model.Email
+                && u.Password == hashedPassword
+                && (u.UserRole == UserRoleConstants.Admin || u.UserRole == UserRoleConstants.Staff || u.UserRole == UserRoleConstants.Manager));
+
+                if (user != null)
+                {
+                    Session[SessionDataKeys.Email] = user.Email;
+                    Session[SessionDataKeys.UserRole] = user.UserRole;
+                    FormsAuthentication.SetAuthCookie(user.Email, true);
+
+                    return RedirectToAction("Index", "AdminAccount");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng.");
+
+                }
+            }
+
+            return View(model);
         }
     }
 }
